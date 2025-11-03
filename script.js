6e96c87a8e226e4946ef3eafd9dd08d503494660
Sina Prj <your-email@example.com>
2025-11-03 17:22:25 +0330

    coding addTodoToList function

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
 let $ = document;
+
+let addBtn = $.querySelector('#addBtn')
+let inputText = $.querySelector('.todo-add-btn')
+let TodoContainer = $.querySelector('.list-container')
+
+
+addBtn.addEventListener('click' , addTodoToList)
+
+
+
+function addTodoToList () {
+    let TodoValue = inputText.value
+
+    let todoHtml = `<li class="todo">
+                        <label>
+                          <input class="todo-checkbox" type="checkbox">
+                          <span class="checkmark"></span>
+                        </label>
+                        <span class="todo-text">${TodoValue}</span>
+                      </li>`
+
+    TodoContainer.insertAdjacentHTML("beforeend" , todoHtml)
+    
+}
