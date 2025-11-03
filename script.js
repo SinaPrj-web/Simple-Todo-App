200d99aaacf847b56c04b3baff46885a503ed5c2
Sina Prj <your-email@example.com>
2025-11-03 18:46:55 +0330

    handle local storage

--- script.js
+++ script.js
@@ -3,23 +3,72 @@ let $ = document;
 let addBtn = $.querySelector('#addBtn')
 let inputText = $.querySelector('.todo-add-btn')
 let TodoContainer = $.querySelector('.list-container')
+let icon = $.querySelector('#plusIcon')
 
 
-addBtn.addEventListener('click' , addTodoToList)
+
+
+window.addEventListener('DOMContentLoaded' , function () {
+    const existingTodos = JSON.parse(localStorage.getItem('todo')) || []
+
+    existingTodos.forEach(function (todo){
+        const todoHtml = `<li class="todo">
+        <label>
+          <input class="todo-checkbox" type="checkbox" ${todo.completed ? 'checked' : ''}>
+          <span class="checkmark"></span>
+          <span class="todo-text">${todo.text}</span>
+        </label>
+      </li>`
+
+      TodoContainer.insertAdjacentHTML('beforeend' , todoHtml)
+    })
+
+})
 
 
 
 function addTodoToList () {
-    let TodoValue = inputText.value
+    let todoValue = inputText.value.trim()
+    const existingTodos = JSON.parse(localStorage.getItem('todo')) || []
+    
+
+    if(todoValue === '') {
+        alert('Please enter a todo first')
+        return
+    }
+    const newObj = {
+        text : todoValue,
+        completed : false
+    }
+    
+    existingTodos.push(newObj)
+    localStorage.setItem('todo' , JSON.stringify(existingTodos))
+     
 
     let todoHtml = `<li class="todo">
                         <label>
                           <input class="todo-checkbox" type="checkbox">
                           <span class="checkmark"></span>
                         </label>
-                        <span class="todo-text">${TodoValue}</span>
+                        <span class="todo-text">${todoValue}</span>
                       </li>`
 
     TodoContainer.insertAdjacentHTML("beforeend" , todoHtml)
+
+    inputText.value = ''
     
 }
+
+addBtn.addEventListener('click' , addTodoToList)
+inputText.addEventListener('keydown' , function (event) {
+    let todoValue = inputText.value.trim()
+    if(event.key === 'Enter' && todoValue !== '') {
+        addTodoToList()
+    }
+})
+
+icon.addEventListener('click' , function() {
+    localStorage.removeItem('todo')
+    TodoContainer.innerHTML = ''
+    
+})
